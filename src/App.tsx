f89b4b6a7d034ee29800514bc5cb2fc33eb221e8
Lucas Isabel <lucasisabel@Lucass-MacBook-Pro.local>
2024-01-08 23:14:51 -0300

    feat: added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Button } from './components/Button';
+import Router from './Router';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/globals';
+import { BrowserRouter as RouterWrapper } from 'react-router-dom';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <RouterWrapper>
+        <Router />
+      </RouterWrapper>
       <GlobalStyle />
     </ThemeProvider>
   );
